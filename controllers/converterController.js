cd8c0cacd042a0b21326b33024129b8f0c527ed4
aaditya45 <sunita45shr@gmail.com>
2024-01-10 19:17:06 +0530

    convert update

--- controllers/converterController.js
+++ controllers/converterController.js
@@ -1,60 +1,82 @@
+const {v4 : uuidv4} = require('uuid');
+
+
 const mangaConversion = (req, res) => {
   const inputJson = req.body;
-  const outputJson = {
-    id: { type: 'String' },
-    mdex_id: { type: 'String' },
-    title: { en: { type: 'String' } },
-    altTitles: { type: ['Mixed'] },
-    description: { en: { type: 'String' } },
-    isLocked: { type: 'Boolean' },
-    originalLanguage: { type: 'String' },
-    lastVolume: { type: 'String' },
-    lastChapter: { type: 'String' },
-    publicationDemographic: { type: 'String' },
-    status: { type: 'String' },
-    year: { type: 'String' },
-    contentRating: { type: 'String' },
-    genre: { type: ['String'] },
-    author: { type: ['String'] },
-    artist: { type: ['String'] },
-    keywords: { type: 'Array' },
-    chapterNumbersResetOnNewVolume: { type: 'Boolean' },
-    createdAt: { type: 'String' },
-    updatedAt: { type: 'String' },
-    latestUploadedChapter: { type: 'Array' },
-    relationship: { type: 'Array' },
-    type: { type: 'String' },
-    views: { type: 'Number' },
-    rating: { type: 'Number' },
-    follows: { type: 'Number' },
+  var outputJson =  outputJson = {
+    id: "",
+    mdex_id: "",
+    title: [],
+    altTitles: {},
+    description: { en: "" },
+    isLocked: false,
+    originalLanguage: "",
+    lastVolume: "",
+    lastChapter: "",
+    publicationDemographic: "",
+    status: "",
+    year: "",
+    contentRating: "",
+    genre: [],
+    author: [],
+    artist: [],
+    keywords: [],
+    chapterNumbersResetOnNewVolume: false,
+    createdAt: "",
+    updatedAt: "",
+    latestUploadedChapter: [],
+    relationship: [],
+    type: "",
+    views: 0,
+    rating: 0,
+    follows: 0,
     poster: {
-      main: { type: 'String' },
-      optimized: { type: 'String' },
-      cdnUrl: { type: 'String' },
+      main: "",
+      optimized: "",
+      cdnUrl: "",
     },
-    Slug: { type: 'String' },
+    Slug: "",
   };
-  outputJson.id.type = inputJson.id.type;
-  outputJson.title.en.type = inputJson.attributes.title.en.type;
-  outputJson.altTitles.type = inputJson.attributes.altTitles.type;
-  outputJson.description.en.type =
-    inputJson.attributes.description?.type || "String";
-  outputJson.isLocked.type = inputJson.attributes.isLocked.type;
-  outputJson.originalLanguage.type = inputJson.attributes.originalLanguage.type;
-  outputJson.lastVolume.type = inputJson.attributes.lastVolume.type;
-  outputJson.lastChapter.type = inputJson.attributes.lastChapter.type;
-  outputJson.publicationDemographic.type =
-    inputJson.attributes.publicationDemographic.type;
-  outputJson.status.type = inputJson.attributes.status.type;
-  outputJson.year.type = inputJson.attributes.year.type;
-  outputJson.contentRating.type = inputJson.attributes.contentRating.type;
-  outputJson.chapterNumbersResetOnNewVolume.type =
-    inputJson.attributes.chapterNumbersResetOnNewVolume.type;
-  outputJson.createdAt.type = inputJson.attributes.createdAt.type;
-  outputJson.updatedAt.type = inputJson.attributes.updatedAt.type;
-  outputJson.latestUploadedChapter.type =
-    inputJson.attributes.latestUploadedChapter.type;
-  outputJson.relationship.type = inputJson.relationships.type;
+  outputJson.id = uuidv4();
+  outputJson.mdex_id = inputJson.id;
+  outputJson.title = inputJson.attributes.title.en;
+  outputJson.altTitles.en = inputJson.attributes.altTitles.en
+  outputJson.description.en = inputJson.attributes.description?.en || "";
+  outputJson.isLocked = inputJson.attributes.isLocked || false;
+  outputJson.originalLanguage = inputJson.attributes.originalLanguage || "";
+  outputJson.lastVolume = inputJson.attributes.lastVolume || "";
+  outputJson.lastChapter = inputJson.attributes.lastChapter || "";
+  outputJson.publicationDemographic = inputJson.attributes.publicationDemographic || null;
+  outputJson.status = inputJson.attributes.status || "";
+  outputJson.year = inputJson.attributes.year || "";
+  outputJson.contentRating = inputJson.attributes.contentRating || "";
+  outputJson.genre = inputJson.attributes.tags
+    .filter((tag) => tag.type === "tag" && tag.attributes.group === "genre")
+    .map((tag) => tag.attributes.name.en);
+  outputJson.author = inputJson.relationships
+    .filter((relation) => relation.type === "author")
+    .map((author) => author.id);
+  outputJson.artist = inputJson.relationships
+    .filter((relation) => relation.type === "artist")
+    .map((artist) => artist.id);
+  outputJson.keywords = inputJson.attributes.tags
+    .filter((tag) => tag.type === "tag" && tag.attributes.group === "theme")
+    .map((tag) => tag.attributes.name.en);
+  outputJson.chapterNumbersResetOnNewVolume = inputJson.attributes.chapterNumbersResetOnNewVolume || false;
+  outputJson.createdAt = inputJson.attributes.createdAt || "";
+  outputJson.updatedAt = inputJson.attributes.updatedAt || "";
+  outputJson.latestUploadedChapter = [inputJson.attributes.latestUploadedChapter];
+  outputJson.relationship = inputJson.relationships.map((relation) => {
+    return { id: relation.id, type: relation.type, related: relation.related || null };
+  });
+  outputJson.type = inputJson.type;
+  outputJson.views = inputJson.views?.type || 0;
+  outputJson.rating = inputJson.rating?.type || 0;
+  outputJson.follows = inputJson.follows?.type || 0;
+  outputJson.poster.main = inputJson.attributes.links?.al || "";
+  outputJson.poster.optimized = inputJson.attributes.links?.ap || "";
+  outputJson.poster.cdnUrl = inputJson.attributes.links?.kt || "";
+  outputJson.Slug = inputJson.attributes.links?.mu || "";
 
   res.send({
     "output": outputJson,
